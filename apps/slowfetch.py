c0217f79433a88a395f21719e9aa5e1ae8b71dcc
Danila <banka228pro@gmail.com>
2026-02-11 22:39:08 +0300

    fixed bug
    The program couldn't find the config from any folder except kernel.

--- apps/slowfetch.py
+++ apps/slowfetch.py
@@ -1,5 +1,6 @@
 import os
-
+from pathlib import Path
+import subprocess
 class Colors:
     BLACK = "\033[0;30m"
     RED = "\033[0;31m"
@@ -21,14 +22,44 @@ class Colors:
 
 class slowfetch_class:
     def __init__(self):
+        self.cfg_path = None
+        self.check_config_path()
         self.OS_NAME=""
-        self.DE=""
         self.VERSION=""
         self.config = {}
-        self.config = self.parse_config('../config/slowfetch.cfg')
+        self.config = self.parse_config(self.cfg_path)
         self.uptime = self.get_uptime()
         self.slowfetch_out()
+    
+    def check_config_path(self):
+        base_text = "OS_NAME=QuickPyre\nVERSION=Release 2.2"
 
+        current_dir = Path.cwd()
+    
+        #ищем файл, поднимаясь вверх по директориям
+        while current_dir:
+
+            for file in current_dir.rglob("config/slowfetch.cfg"):
+                if file.exists():
+                    #файл найден -- запускаем
+                    self.cfg_path = Path(os.path.abspath(str(file)))
+                    return
+            
+            #если папка щас это папка проекта, а cfg не найден: 
+            if current_dir.name == "QuickPyre":
+                break
+            
+            
+            if current_dir.parent == current_dir:  #если программа уже в корне: выводим что файла нет.
+                break
+            
+            current_dir = current_dir.parent #идем на папку выше
+        if not cfg_path.exists():
+            try:
+                with open(cfg_path, "w") as f:
+                    f.write(base_text)
+            except Exception as e:
+                print(f"Error in writing config file on path QuickPyre/config/slowfetch.cfg: {e}")
     def get_uptime(self):
         try:
             with open('/proc/uptime') as f:
@@ -57,7 +88,7 @@ class slowfetch_class:
                 # Разделяем по первому знаку '='
                 if '=' in line:
                     key, value = line.split('=', 1)
-                    self.config[key.strip()] = value.strip()
+                    self.config[key.strip()] = str(value).strip()
 
         return self.config
 
@@ -65,16 +96,7 @@ class slowfetch_class:
     def slowfetch_out(self):
         global config
         self.OS_NAME = self.config.get('OS_NAME')
-        self.DE = self.config.get('DE')
         self.VERSION = self.config.get('VERSION')
-        #print(self.OS_NAME, self.WM, self.VERSION)
-        #print(f" ___        	    ___          ")
-        #print(f"/   \\       	   /   \\         {Colors.LIGHT_GREEN}OS{Colors.END}: {self.OS_NAME}")
-        #print(f"|   |              |   |         {Colors.LIGHT_GREEN}Kernel{Colors.END}: In Development...")
-        #print(f"| @ |   QuickPyre  | @ |         {Colors.LIGHT_GREEN}Uptime{Colors.END}: {self.uptime}")
-        #print(f"|   |              |   |         {Colors.LIGHT_GREEN}DE{Colors.END}: {self.DE}")
-        #print(f"\\___/     \\_|_/    \\___/         {Colors.LIGHT_GREEN}Version{Colors.END}: {self.VERSION}")
-        #print(f"                                 {Colors.LIGHT_GREEN}Shell{Colors.END}: In Development...")
         print(f"           . ..:.")
         print(f"           .+@..+.")
         print(f"           ++# ***..")
@@ -83,9 +105,9 @@ class slowfetch_class:
         print(f"      =   %++%*++=+.:               {Colors.LIGHT_GREEN}OS{Colors.END}: {self.OS_NAME}")
         print(f"      =-... @*=-=+*#@+.             {Colors.LIGHT_GREEN}Kernel{Colors.END}: In Development...")
         print(f"     %.-=*:-=@@@@@@@%*==.           {Colors.LIGHT_GREEN}Uptime{Colors.END}: {self.uptime}")
-        print(f"    -#**%:*@         @*-=.          {Colors.LIGHT_GREEN}DE{Colors.END}: {self.DE}")
-        print(f"  .-+-====@  @@   .   #...*--       {Colors.LIGHT_GREEN}Version{Colors.END}: {self.VERSION}")
-        print(f"  --=.+@@@@@%::@:     @@@@%*..      {Colors.LIGHT_GREEN}Shell{Colors.END}: In Development...")
+        print(f"    -#**%:*@         @*-=.          {Colors.LIGHT_GREEN}Version{Colors.END}: {self.VERSION}")
+        print(f"  .-+-====@  @@   .   #...*--       {Colors.LIGHT_GREEN}Shell{Colors.END}: In Development...")
+        print(f"  --=.+@@@@@%::@:     @@@@%*..")
         print(f" .=#%@@          :::: @    @+-.")   
         print(f".:#*=@  -  .....      @ ::  @=*.    {Colors.CYAN}██{Colors.BLUE}██{Colors.PURPLE}██" f"{Colors.GREEN}██{Colors.END}")
         print(f" =#++@        .. .. @@      @:+-    {Colors.YELLOW}██{Colors.RED}██{Colors.END}")
